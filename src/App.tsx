9e81f051d2b57113f221a5329cb601d45b4bcdc9
Hemanth <hemanththyagaraj@gmail.com>
2023-03-08 19:23:45 +0530

    Adds: Sass setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,7 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto necessitatibus, assumenda dolor veniam non sequi maiores rerum sit provident libero adipisci quod facilis numquam repudiandae? Cupiditate doloremque voluptas repellendus asperiores qui nulla blanditiis non vero modi beatae ipsum itaque fuga veritatis veniam nostrum, in ut eum sequi quisquam esse vel quaerat consectetur. Doloribus odit maiores doloremque animi atque, non iste cumque iusto asperiores accusantium dolore nisi sed reiciendis consequatur natus velit quos, distinctio quas fugit sint, laborum ipsam quia aut ipsa? Unde aliquam ratione omnis tenetur ad corrupti, sint eum nisi adipisci odio, consequuntur eos sit dolores! Quae id explicabo nostrum animi maiores molestias odit, et dolorum, dolore magni non vero quibusdam velit? Repellat, perspiciatis? Repellat reiciendis repellendus, sunt error ad dignissimos neque perspiciatis eos id porro tempore quis dolore temporibus saepe deserunt veritatis numquam molestiae inventore accusantium officiis voluptate, ab hic. Obcaecati reiciendis blanditiis et ullam accusantium amet porro commodi deleniti totam doloribus tempore excepturi magni, asperiores atque quaerat rem hic doloremque maiores? Vel illo perferendis exercitationem. Vel eum natus tenetur totam, rem delectus porro! Asperiores, ut sunt veniam consequatur exercitationem mollitia aliquid soluta eligendi sit consectetur inventore dolor quidem voluptates a laborum illo autem neque aperiam molestiae earum.
     </div>
   );
 }
